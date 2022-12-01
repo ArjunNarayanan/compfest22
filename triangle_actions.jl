fd8f2281d0bfda768dda0a010ce4659c83269711
Arjun <arjun.narayanan91@gmail.com>
2022-11-30 16:58:50 -0800

    added triangle collapse figure

--- triangle_actions.jl
+++ triangle_actions.jl
@@ -41,5 +41,19 @@ fig.savefig("figures/triangle-split1.png")
 ##
 
 ##
+vertices = [0.  0.  0.5 1.  1.  1.5 2.  2.
+            -0.5    0.5 0.  -0.5    0.5 0.  -0.5    0.5]
+connectivity = [1 1 2 3 3 6 6 6
+                3 4 3 4 6 4 8 7
+                2 3 5 6 5 7 5 8]
+mesh = TM.Mesh(vertices, connectivity)
+fig = MP.plot_mesh(TM.active_vertex_coordinates(mesh), TM.active_triangle_connectivity(mesh))
+fig.tight_layout()
+fig.savefig("figures/triangle-collapse0.png")
 
+TM.collapse!(mesh, 4, 2)
+TM.reindex!(mesh)
+fig = MP.plot_mesh(TM.active_vertex_coordinates(mesh), TM.active_triangle_connectivity(mesh))
+fig.tight_layout()
+fig.savefig("figures/triangle-collapse1.png")
 ##
